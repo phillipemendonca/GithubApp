cca89ca12ecd8a1f1f848aa70db2b0866678761f
Phillipe Mendonça <phillipe.mendonca20@gmail.com>
2022-10-29 19:16:26 -0300

    Updates

---
+++ precache-manifest.7bcff0da47d98ad4d51544bfb2cee932.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "df71013d8a6cff724538",
+    "url": "/GithubApp/static/js/main.df71013d.chunk.js"
+  },
+  {
+    "revision": "567941eab0139feb58c5",
+    "url": "/GithubApp/static/js/1.567941ea.chunk.js"
+  },
+  {
+    "revision": "fe49bfeffb1f7e8660bc",
+    "url": "/GithubApp/static/js/runtime~main.fe49bfef.js"
+  },
+  {
+    "revision": "804934b7021427b7f1bdeed142c88515",
+    "url": "/GithubApp/index.html"
+  }
+];
